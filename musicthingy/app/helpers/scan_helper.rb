73cd2fc8899dd3f0f2933f450b8c03aaa87eecb0
Jesse Lingeman <jesse@Frohike.(none)>
2011-02-06 20:44:36 -0500

    Fixed most of the bugs in scan_helper.  It will not get thru a fairly large database of a couple hundred test files.  Still need to add file moving and database building, but those should be trivial functions.  Good enough right now to maybe start building some other components that will work with it.

--- musicthingy/app/helpers/scan_helper.rb
+++ musicthingy/app/helpers/scan_helper.rb
@@ -1,78 +1,126 @@
+# encoding: utf-8
 module ScanHelper
+
     class Scanner
         def scan_folder(folder)
             require 'rbrainz'
             require 'find'
             require 'taglib'
 
-            mb_query = MusicBrainz::Webservice::Query.new
 
             artist_cache = Hash.new
             album_cache = Hash.new
 
             Find.find(folder) do |entry|
-                if entry.downcase.include?(".mp3")
-                    puts entry
-                    tag = TagLib::File.new(entry)
+                begin
+                    if entry[-4..-1].downcase.include?(".mp3")
 
-                    artist_tag = tag.artist.to_s
-                    album_tag = tag.album.to_s
-                    track_tag = tag.title.to_s
-                    # Check if we already have this album and artist uuid in
-                    # cache to minimize queries to musicbrainz
+                        sleep(1) # To comply with MusicBrainz restrictions
 
-                    if artist_cache[artist_tag] != nil
-                        artist_name, artist_id, artist_score = artist_cache[artist_tag]
-                    else
-                        artist_name, artist_id, artist_score = self.find_artist(mb_query, artist_tag)
-                        artist_cache[artist_tag] = artist_name, artist_id, artist_score
+                        mb_query = MusicBrainz::Webservice::Query.new
+                        puts entry
+                        # Check if we already have this album and artist uuid in
+                        # cache to minimize queries to musicbrainz
+
+                        artist_tag, album_tag, track_tag = get_tags(entry)
+
+                        if artist_cache[artist_tag] != nil
+                            artist_name, artist_id, artist_score = artist_cache[artist_tag]
+                        elsif artist_tag != nil and artist_tag.strip() != ""
+                            artist_name, artist_id, artist_score = self.find_artist(mb_query, artist_tag)
+                            artist_cache[artist_tag] = artist_name, artist_id, artist_score
+                        else
+                            artist_id = nil
+                        end
+
+                        if artist_id != nil and artist_score > 70
+                            new_artist_tag = artist_name
+                        else
+                            puts "Too unsure of artist name.  Not tagging."
+                            next
+                        end
+
+                        if album_cache[album_tag] != nil
+                            album_name, album_id, album_score = album_cache[album_tag]
+                        elsif album_tag != nil and album_tag.strip() != ""
+                            album_name, album_id, album_score = self.find_album(mb_query, artist_id, album_tag)
+                            album_cache[album_tag] = album_name, album_id, album_score
+                        else
+                            album_id = nil
+                        end
+
+                        if album_id != nil and album_score > 70
+                            new_album_tag = album_name
+                        else
+                            puts "Too unsure of album name.  Not tagging."
+                            next
+                        end
+
+                        # Get track from MB
+                        track_name, track_id, track_score = self.find_track(mb_query, album_id, track_tag)
+
+                        if track_id != nil and track_score > 70
+                            new_track_tag = track_name
+                        else
+                            puts "Too unsure of track name.  Not tagging."
+                            next
+                        end
+
+                        set_tags( entry, new_artist_tag, new_album_tag, new_track_tag )
+
                     end
-
-                    if artist_id != nil and artist_score > 70
-                        tag.artist = artist_name
-                        tag.save
-                    else
-                        puts "Too unsure of artist name.  Not tagging."
-                        next
-                    end
-
-                    if album_cache[album_tag] != nil
-                        album_name, album_id, album_score = album_cache[album_tag]
-                    else
-                        album_name, album_id, album_score = self.find_album(mb_query, artist_id, album_tag)
-                        album_cache[album_tag] = album_name, album_id, album_score
-                    end
-
-                    if album_id != nil and album_score > 70
-                        tag.album = album_name
-                        tag.save
-                    else
-                        puts "Too unsure of album name.  Not tagging."
-                        next
-                    end
-
-                    # Get track from MB
-                    track_name, track_id, track_score = self.find_track(mb_query, artist_id, album_id, track_tag)
-
-                    if track_id != nil and track_score > 70
-                        tag.title = track_name
-                        tag.save
-                    else
-                        puts "Too unsure of track name.  Not tagging. Score: %i" % track_score
-                        next
-                    end
+                    #@artist = Artist.new(:name => entry, :path => folder+"/"+entry)
+                rescue MusicBrainz::Webservice::RequestError
+                    puts "Warning: Unable to read tags for file: %s, skipping." % entry
                 end
-                #@artist = Artist.new(:name => entry, :path => folder+"/"+entry)
                 #@artist.save
                 #puts @artist.errors
             end
         end
 
+        def get_tags(file)
+            tag = TagLib::File.new(file)
+
+            artist_tag = self.format_string(tag.artist.to_s)
+            album_tag = self.format_string(tag.album.to_s)
+            track_tag = self.format_string(tag.title.to_s)
+
+            # Have this be a regex string in the config file somewhere so the
+            # user can set up how the directory is listed.
+
+            if track_tag.strip() == ""
+                track_tag = file[file.rindex("/")+1, -5]
+                tag.title = track_tag
+                tag.save()
+            end
+            tag.close()
+
+            return artist_tag, album_tag, track_tag
+        end
+
+        def set_tags(file, artist_tag, album_tag, track_tag)
+            tag = TagLib::File.new(file)
+
+            tag.artist = artist_tag
+            tag.album = album_tag
+            tag.track = track_tag
+
+            tag.save()
+            tag.close()
+
+        end
+
+        def format_string(str)
+            str.sub!("/"," ")
+            str.strip!()
+            return str
+        end
 
         def find_artist(mb_query, artist_name)
 
             puts "Looking up artist in MusicBrainz artist entry for %s" % artist_name
             artist_filter = MusicBrainz::Webservice::ArtistFilter.new(:name => artist_name)
+            puts artist_filter
 
             artists = mb_query.get_artists(artist_filter)
 
@@ -107,12 +155,11 @@ module ScanHelper
             return new_title, album_id.uuid, album.score
         end
 
-        def find_track(mb_query, artist_id, album_id, track_name)
+        def find_track(mb_query, album_id, track_name)
 
             puts "Fetching track data"
             track_filter = MusicBrainz::Webservice::TrackFilter.new(
                 :releaseid => album_id,
-                :artistid => artist_id,
                 :title => track_name
             )
 
@@ -128,7 +175,7 @@ module ScanHelper
             score = track.score
 
             puts track_id, new_title, score
-            puts "Found track %s, compared to current track %s" % [track.entity.title, track_name]
+            #puts "Found track %s, compared to current track %s" % [track.entity.title, track_name]
             return new_title, track_id.uuid, score
 
         end
